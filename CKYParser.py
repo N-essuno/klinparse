eed21b9e79cd099f02cbd0046d25297b4bb7dfa3
Gianluca Bàrmina <70454516+N-essuno@users.noreply.github.com>
2023-11-21 11:05:15 +0100

    Implemented first version of CKY
    Co-authored-by: Marco Amato <MarcoAmato@users.noreply.github.com>

--- CKYParser.py
+++ CKYParser.py
@@ -11,5 +11,18 @@ class CKYParser:
         :param words: The words to parse.
         :return: A matrix representing the possible parses of the given words.
         """
-        raise NotImplementedError
+        n = len(words)
+        # Initialize the matrix.
+        matrix = [[[] for _ in range(n + 1)] for _ in range(n + 1)]
+        # Fill the matrix.
+        for j in range(1, n + 1):
+            matrix[j - 1][j] = [tag for tag, words in self._grammar.items() if words[0] == words[j - 1]]
+            for i in range(j - 2, -1, -1):
+                for k in range(i + 1, j):
+                    for tag, words in self._grammar.items():
+                        if words[0] == words[j - 1]:
+                            for left in matrix[i][k]:
+                                for right in matrix[k][j]:
+                                    matrix[i][j].append(tag)
+        return matrix
 
