2cb1cdba7cbec22fdf5570234a1feed35bbe367f
marcooamato <amatomarco111@gmail.com>
2023-11-21 14:07:14 +0100

    grammar is a list but there's a bug
    Co-authored-by: Gianluca Bàrmina <N-essuno@users.noreply.github.com>

--- CKYParser.py
+++ CKYParser.py
@@ -11,31 +11,31 @@ class CKYParser:
         :param words: The words to parse.
         :return: A matrix representing the possible parses of the given words.
         """
-        n = len(words) - 1
+        n = len(words)
         # Initialize the matrix.
-        matrix = [[[] for _ in range(n + 1)] for _ in range(n + 1)]
+        table = [[[] for _ in range(n)] for _ in range(n)]
         # Fill the matrix.
-        for j in range(0, n+1):
+        for j in range(0, n):
             # Fill the diagonal first. Contains the constituents for the various words.
             # We don't need to check if the derivation is only one word (terminal),
             #   because we know that the grammar is in CNF.
-            matrix[j][j] = [tag for tag, derivation in self._grammar.items() if derivation[0] == words[j]]
+            table[j][j] = [tag for tag, derivation in self._grammar if derivation[0] == words[j]]
 
             # Fill the rest of the matrix. For each column j, we iterate over the rows i in reverse order.
             for i in range(j - 2, -1, -1):
                 # Iterate over the possible splits.
-                for k in range(i + 1, j):
+                for k in range(i+1, j+1):
                     # Iterate over the possible derivations.
-                    for tag, derivation in self._grammar.items():
+                    for tag, derivation in self._grammar:
                         # If the rule brings to a terminal (len(derivation) == 1), skip it.
                         if len(derivation) == 2:
                             left = derivation[0]
                             right = derivation[1]
                             # Check if the derivation rule has as left child the tag in matrix[i][k] and as right child
                             #   the tag in matrix[k][j]. If so, add the tag to matrix[i][j].
-                            if left in matrix[i][k] and right in matrix[k][j]:
-                                matrix[i][j].append(tag)
-        return matrix
+                            if left in table[i][k] and right in table[k][j]:
+                                table[i][j].append(tag)
+        return table
 
 
 def print_cky_matrix_pretty():
@@ -45,20 +45,25 @@ def print_cky_matrix_pretty():
 
 
 if __name__ == "__main__":
-    grammar = {
-        "S": ["NP", "VP"],
-        "NP": ["Det", "N"],
-        "VP": ["V", "NP"],
-        "Det": ["a"],
-        "N": ["dog", "cat"],
-        "V": ["chased", "sat"]
-    }
-    # TODO Grammar must be list of tuples instead of dict, correct
+    grammar = [
+        ("S", ["NP", "VP"]),
+        ("NP", ["Det", "N"]),
+        ("VP", ["V", "NP"]),
+        ("PP", ["P", "NP"]),
+        ("NP", ["NP", "PP"]),
+        ("Det", ["a"]),
+        ("Det", ["the"]),
+        ("N", ["dog"]),
+        ("N", ["cat"]),
+        ("N", ["rug"]),
+        ("V", ["chased"]),
+        ("P", ["in"]),
+        ("P", ["on"]),
+        ("P", ["by"])
+    ]
     parser = CKYParser(grammar)
     # print matrix in a pretty way
     matrix = parser.parse(["a", "dog", "chased", "a", "cat"])
     for row in matrix:
         print(row)
-    # print the parses
-    print(matrix[0][len(matrix) - 1])
 
